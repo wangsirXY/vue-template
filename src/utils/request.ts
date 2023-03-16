fb3f2910d234564fa0e5572dc508c5fb16a09146
GuAn <15713912912@163.com>
2023-03-16 11:15:53 +0800

    fix: :pencil2: 修复axios请求拦截器类型问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -9,7 +9,7 @@ import type { AxiosRequestConfig, InternalAxiosRequestConfig, AxiosResponse } fr
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_API,
+  baseURL: process.env.VITE_APP_BASE_API,
   timeout: 6000,
   headers: { "Content-Type": "application/json;charset=utf-8" },
 });
